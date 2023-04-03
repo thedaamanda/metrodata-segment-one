df1df4040c116fbf5293f7dd469ca2ad0317ac05
thedaamanda <thedaamandaa@gmail.com>
2023-04-03 17:12:45 +0700

    feat: add context to handle database connection

---
+++ SegmentOne/Contexts/MyContext.cs
@@ -0,0 +1,57 @@
+﻿using System.Data.SqlClient;
+
+namespace SegmentOne.Contexts
+{
+	public class MyContext
+	{
+        private static SqlConnection connection;
+        private string connectionString;
+
+		public MyContext(string serverName, string databaseName, string userName, string password)
+		{
+            // Memasukkan data koneksi ke dalam string
+            connectionString = "Server=" + serverName + ";Database=" + databaseName + ";User Id=" + userName + ";Password=" + password + "; Trusted_Connection=False; MultipleActiveResultSets=true";
+
+            // Menginisialisasi SqlConnection object
+            connection = new SqlConnection(connectionString);
+		}
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                // Membuka koneksi ke database
+                connection.Open();
+                Console.WriteLine("Connection Open ! ");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // Melakukan penanganan error jika gagal membuka koneksi
+                Console.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+        }
+
+        public void CloseConnection()
+        {
+            // Menutup koneksi ke database
+            connection.Close();
+        }
+
+        public SqlConnection GetConnection()
+        {
+            try
+            {
+                // Mengembalikan SqlConnection object
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                // Melakukan penanganan error jika gagal mengembalikan SqlConnection object
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+	}
+}
--- SegmentOne/Program.cs
+++ SegmentOne/Program.cs
@@ -1,7 +1,12 @@
-﻿namespace SegmentOne
+﻿using SegmentOne.Contexts;
+
+namespace SegmentOne
 {
     class Program
     {
+        // membuat objek MyContext
+        private static MyContext connection = new MyContext("localhost", "db_hr_dts", "sa", "Passw@rd");
+
         static void Main(string[] args)
         {
 
